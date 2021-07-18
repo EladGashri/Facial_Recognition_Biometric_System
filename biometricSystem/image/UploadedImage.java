fdbd6f5c1066e8e262b54cd2e27d3c40646e082f
Elad Gashri <Eladgashri@gmail.com>
2021-07-18 13:58:19 +0300

    Update UploadedImage.java

--- biometricSystem/image/UploadedImage.java
+++ biometricSystem/image/UploadedImage.java
@@ -5,7 +5,6 @@ import org.opencv.imgcodecs.Imgcodecs;
 
 public class UploadedImage extends ImageForIdentification {
 
-
     public UploadedImage(byte[] bytes){
         super(Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.IMREAD_UNCHANGED));
     }
