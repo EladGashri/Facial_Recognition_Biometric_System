b1ff279703af96926456fa80554b1d4588b8e943
Elad Gashri <Eladgashri@gmail.com>
2021-10-14 21:43:09 +0300

    Update new_model.py

--- python/new_model.py
+++ python/new_model.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
 
-    ann = Ann(batch_size=30, epochs=20, lr=0.0001, l2=0.001)
+    ann = Ann(batch_size=30, epochs=30, lr=0.0001, l2=0.001)
     ann.main()
     ann.save_model(sys.argv[1])
     traced_cell = torch.jit.trace(ann.model, torch.rand(1,3,160,160).to(ann.device))
